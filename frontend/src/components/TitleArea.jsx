4117bb9786a0bdcad9bd10cf237b84acd574b0ca
Griffin Robinson <78376643+GriffRob1@users.noreply.github.com>
2025-04-14 19:41:27 -0400

    finished title area

--- frontend/src/components/TitleArea.jsx
+++ frontend/src/components/TitleArea.jsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import dna_code_image from '../images/dna-code-image.jpg'
+import { useNavigate } from "react-router-dom";
 
 const sectionStyle = {
     backgroundImage: 'url(' + dna_code_image + ')',
 }
 
 export default function TitleArea() {
+
+    const navigate = useNavigate();
+    const navigateToAbout = () =>{
+        navigate('/about');
+    }
+
     return (
         <section className='container title-container' style={sectionStyle}>
             <div className='title-area'>
                 <h1 className='title'>Bioinformatics Tools</h1>
+                <p>Essential tools for analysing biological data</p>
+                <button className="container" onClick={navigateToAbout}>What is Bioinformatics?</button>
             </div>
         </section>
     )
